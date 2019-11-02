faabe5b46dd5a1f364aee7728beea6937aeb39de
Roma Makarov <r0mmm4k@gmail.com>
2019-11-03 00:38:41 +0300

    - add new logo

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import logo from './logo.png'
+import logo from './logo.svg'
 
 const Header = ({decFont, incFont}) => {
 	return (
 		<header className='navbar navbar-dark bg-dark pt-0 pb-0'>
 				<a  href='/' className='navbar-brand'>
-					<img src={logo} className=' mb-2' alt='logo'/>
+					<img src={logo} width={35} className=' mb-2' alt='logo'/>
 					<span className='ml-1'>Weather</span>
 				</a>
 			<div>
-				<button type="button"  onClick={decFont} className="btn btn-info mr-2">--</button>
-				<button type="button" onClick={incFont} className="btn btn-info">+</button>
+				<button type="button"  onClick={decFont} className="btn btn-outline-success mr-2">--</button>
+				<button type="button" onClick={incFont} className="btn btn-outline-success">+</button>
 			</div>
 		</header>
 	);
